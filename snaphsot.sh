6c9fd04e01509904f3accf8507dce36b69087862
mikimuster <93078189+mikimuster@users.noreply.github.com>
2022-08-14 18:57:55 +0200

    Added Container support

--- snaphsot.sh
+++ snaphsot.sh
@@ -10,35 +10,65 @@ trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 
 keep_snapshots=4
-var=$(sudo btrfs subvolume list /mnt/main | grep nextcloud | wc -l)
 snapshot_name=$( date '+%F_%H:%M:%S' )
 data_dir="/mnt/main/data"
 
+echo 
+echo =========================================
 echo
-echo  Starting snapshoting...
+echo  Starting snapshoting
+echo
 
 for i in "$@"
 do
-    echo  Processing $i...
     x=$i
     z=$i
-    execute="sudo btrfs subvolume snapshot $data_dir/$i $data_dir/.snapshots/$x$snapshot_name"
+    g=$i
+    d=$i
+    t=$i
+
+    echo -----------------------------------------
+    echo Processing $i...
+    echo 
+    echo Stoping containers:
+    echo 
+    
+    processes=$(docker ps | grep $g | awk  '{print $1}')
+    #echo $processes
+    processes=($processes)
+    for (( i=0; i<${#processes[@]}; i++ ))
+    do
+        docker container stop "${processes[$i]}"
+        echo Container "${processes[$i]}" has been stoped
+    done
+    echo 
+    
     snapshot_list=$(sudo btrfs subvolume list /mnt/main | grep ".snapshots/$z")
     snapshot_number=$(sudo btrfs subvolume list /mnt/main | grep ".snapshots/$z" | wc -l)
     snapshot_files={}
-
+    
     if (( $snapshot_number >= $keep_snapshots));
     then
         echo "Number of kept snapshots has been reached:" $keep_snapshots
         oldest_snapshot=$(ls -ltr /mnt/main/data/.snapshots/ | grep $x | tail -1 | awk '{print $9}')
         echo Deleting oldest snapshot: $oldest_snapshot
-        sudo btrfs subvolume delete $data_dir/.snapshots/$oldest_snapshot
+        sudo btrfs subvolume delete $data_dir/.snapshots/$oldest_snapshot 
     fi
-    echo
+    echo 
     echo Creating snapshot...
-    sudo btrfs subvolume snapshot $data_dir/$i $data_dir/.snapshots/$x$snapshot_name
-    echo
+    sudo btrfs subvolume snapshot $data_dir/$d $data_dir/.snapshots/$d$snapshot_name
+    echo Snapshot $x$snapshot_name has been created
+    
+    for (( i=0; i<${#processes[@]}; i++ ))
+    do
+        docker start "${processes[$i]}"
+        echo Container "${processes[$i]}" has been restarted
+    done
+
+    echo -----------------------------------------
 done
 
 echo
 echo Backuping process finished
+echo =========================================
+echo
